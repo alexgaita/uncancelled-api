e45a15e3573e21af995a6b8b09539f3cfc81dd72
Mihai Burada <mihai.burada@doctarigroup.com>
2023-05-13 22:39:11 +0300

    updated chat gpt

--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -1,7 +1,8 @@
 import { HttpService } from '@nestjs/axios';
 import { HttpException, Injectable } from '@nestjs/common';
+import * as process from 'process';
 
-const CHAT_GPT_URL = 'https://webhook.site/36d12604-0f23-4fbf-b540-12c52ff4e8b9';
+const CHAT_GPT_URL = 'https://api.openai.com/v1/completions';
 export enum speakingStyle {
   british = 'british',
 }
@@ -13,26 +14,40 @@ export type GenerateSettings = {
   isPixelArt: boolean;
   language: string;
   style: speakingStyle;
-}
+};
 
 @Injectable()
 export class ChatgptService {
-    constructor(private http: HttpService) {}
-    
-    async generateDescription(
-        _seasonId: string,
-        _settings: GenerateSettings,
-      ): Promise<string> {
-        console.log(_seasonId);
-        console.log(_settings);
-        const response = await this.http
-          .get(CHAT_GPT_URL)
-          .toPromise()
-          .catch((err) => {
-            throw new HttpException(err.response.data, err.response.status);
-          });
-    
-        return response.data;
-        // return this.http.get(CHAT_GPT_URL).pipe(map((response) => response.data));
-      }
+  constructor(private http: HttpService) {}
+
+  async generateDescription(
+    _seasonId: string,
+    _settings: GenerateSettings,
+  ): Promise<string> {
+    console.log(_seasonId);
+    console.log(_settings);
+
+    const chatGPTData = {
+      model: 'text-davinci-003',
+      prompt,
+      temperature: 1,
+      max_tokens: 100,
+    };
+    const apiKey = process.env.OPENAI_API_KEY;
+    const reqHeader = {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${apiKey}`,
+      },
+    };
+    const response = await this.http
+      .post(CHAT_GPT_URL, chatGPTData)
+      .toPromise()
+      .catch((err) => {
+        throw new HttpException(err.response.data, err.response.status);
+      });
+
+    return response.data;
+    // return this.http.get(CHAT_GPT_URL).pipe(map((response) => response.data));
+  }
 }
