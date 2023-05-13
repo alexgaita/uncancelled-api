1e9be26ad201ca9ab8cd187e5d6f5915f29e8370
Andi Craiovan <apc@Andis-MacBook-Pro.local>
2023-05-13 22:49:18 +0300

    apc: added new columns

--- src/scenario/entity/scenario.model.ts
+++ src/scenario/entity/scenario.model.ts
@@ -1,4 +1,4 @@
-import { BelongsTo, Column, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
+import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
 import { Paragraph } from "src/paragraph/entity/paragraph.model";
 import { Seasons } from "src/seasons/entity/seasons.model";
 
@@ -10,6 +10,42 @@ export class Scenario extends Model<Scenario> {
 
     @Column
     storyName: string;
+
+    @Column
+    like: number;
+
+    @Column
+    dislike: number;
+
+    @Column
+    violence: number;
+
+    @Column
+    romance: number;
+
+    @Column
+    funny: number;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: false
+    })
+    isInstructional: boolean;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: false
+    })
+    isPixelart: boolean;
+
+    @Column
+    language: string;
+
+    @Column({
+        type: DataType.STRING,
+        defaultValue: "BRITISH"
+    })
+    style: string;
 
     @HasMany(() => Paragraph)
     paragraph: Paragraph[];
